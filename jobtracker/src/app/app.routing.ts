8948cad6e7dbe516ea2da29a895489a215807022
John Hamm <60947846+gohammdata@users.noreply.github.com>
2020-02-22 15:30:25 -0500

    Register Router and Routes for Main Page

--- jobtracker/src/app/app.routing.ts
+++ jobtracker/src/app/app.routing.ts
@@ -1,5 +1,28 @@
-import { mainComponent } from "/main.component";
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { FormsModule }          from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent }          from './app.component';
+import { MainComponent } from "./main/main.component";
 
 export const AvailableRoutes: any = [
-    { path: "main", component: mainComponent },
+    { path: "main", component: MainComponent },
 ];
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
+  declarations: [
+    AppComponent,
+    MainComponent,
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
