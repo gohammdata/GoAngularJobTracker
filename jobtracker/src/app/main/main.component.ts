7900fe8dfc4051cd4b421a4f587cf78f1c7355c5
John Hamm <60947846+gohammdata@users.noreply.github.com>
2020-02-22 16:14:53 -0500

    Edit main.component.ts to include HTTP, Router, and Location imports

--- jobtracker/src/app/main/main.component.ts
+++ jobtracker/src/app/main/main.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';s
+
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
+
 export class MainComponent implements OnInit {
 
   constructor() { }
