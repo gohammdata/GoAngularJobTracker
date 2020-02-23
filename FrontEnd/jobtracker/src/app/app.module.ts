9efa2f11d611b6080d76e5fb97909d601a9a6bcd
John Hamm <60947846+gohammdata@users.noreply.github.com>
2020-02-23 10:17:38 -0500

    Frontend to a working Bootstrap App

--- FrontEnd/jobtracker/src/app/app.module.ts
+++ FrontEnd/jobtracker/src/app/app.module.ts
@@ -4,30 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const appRoutes: Routes = [
-    { path: 'main', component: Main },
-    { path: '', redirectTo: '/main', pathMatch: 'full' },
-    { path: '**', component: PageNotFoundComponent }
-
-]
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MainComponent,
-    PageNotFoundComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(
-    appRoutes,
-    { enableTracing: true} // <-- Debugging purposes
-    )
   ],
   providers: [],
   bootstrap: [AppComponent]
