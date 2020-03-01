b8b9b03c655a4997f26be4df94585b1f0fa6c843
John Hamm <60947846+gohammdata@users.noreply.github.com>
2020-03-01 18:57:51 -0500

    Angular 9 update

--- FrontEnd/jobtracker/src/app/app.component.ts
+++ FrontEnd/jobtracker/src/app/app.component.ts
@@ -1,23 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import { Router } from '@angular/router';
-import { Location } from '@angular/common';
-import { map } from 'rxjs/operators';
-import { ApiService } from './api.service';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-posts;
-constructor(private apiService: ApiService){ }
-ngOnInit(){
-    this.apiService.getPosts().subscribe((data)=>{
-    console.log(data);
-    this.posts = data['posts'];
-    })
+export class AppComponent {
+  title = 'jobtracker';
 }
-}
-
